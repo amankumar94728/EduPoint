25b4b120bed282ef7284137d0bafa8618591eea5
AMAN KUMAR <amankumar@AMANs-MacBook-Air.local>
2026-03-27 21:48:06 +0530

    allow cors

--- backend/utils/mailSender.js
+++ backend/utils/mailSender.js
@@ -11,7 +11,7 @@ const mailSender = async (email, title, body) => {
         });
 
         const info = await transporter.sendMail({
-            from: 'StudyNotion || by Aman Kumar <mailforproject7206@gmail.com>',
+            from: 'EduPoint || by Aman Kumar <mailforproject7206@gmail.com>',
             to: email,
             subject: title,
             html: body
