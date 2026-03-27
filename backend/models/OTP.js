0db21fc1e397b2eaabeefbfd4e7d524f3edd1da8
AMAN KUMAR <amankumar@AMANs-MacBook-Air.local>
2026-03-27 11:15:56 +0530

    first commit

---
+++ backend/models/OTP.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const mailSender = require('../utils/mailSender');
+
+const OTPSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    otp: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+        expires: 10 * 60, // The document will be automatically deleted after 10 minutes of its creation time
+    }
+
+});
+
+//  function to send email
+async function sendVerificationEmail(email, otp) {
+    try {
+        const mailResponse = mailSender(email, 'Verification Email from StudyNotion', otp);
+        console.log('Email sent successfully to - ', email);
+
+    }
+    catch (error) {
+        console.log('Error while sending an email to ', email);
+        throw error;
+    }
+}
+
+// pre middleware
+OTPSchema.pre('save', async function (next) {
+    // console.log("New document saved to database");
+
+    // Only send an email when a new document is created
+    if (this.isNew) {
+        await sendVerificationEmail(this.email, this.otp);
+    }
+    next();
+})
+
+
+
+module.exports = mongoose.model('OTP', OTPSchema);
---
+++ backend/utils/imageUploader.js
@@ -0,0 +1,26 @@
+const cloudinary = require('cloudinary').v2;
+
+exports.uploadImageToCloudinary = async (file, folder, height, quality) => {
+    const options = { folder };
+    if (height) options.height = height;
+    if (quality) options.quality = quality;
+    options.resource_type = 'auto';
+    return await cloudinary.uploader.upload(file.tempFilePath, options);
+}
+
+
+
+// Function to delete a resource by public ID
+exports.deleteResourceFromCloudinary = async (url) => {
+    if (!url) return;
+
+    try {
+        const result = await cloudinary.uploader.destroy(url);
+        console.log(`Deleted resource with public ID: ${url}`);
+        console.log('Delete Resourse result = ', result)
+        return result;
+    } catch (error) {
+        console.error(`Error deleting resource with public ID ${url}:`, error);
+        throw error;
+    }
+};
