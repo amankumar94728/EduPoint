8ae63185bf516a8f443825e8cd7b150f54d53cc0
AMAN KUMAR <amankumar@AMANs-MacBook-Air.local>
2026-04-03 10:36:49 +0530

    google signup signin  setup

--- frontend/src/config/firebase.js
+++ frontend/src/config/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app'
-import { getAuth, GoogleAuthProvider } from 'firebase/auth'
+import { getAuth, GoogleAuthProvider, browserLocalPersistence, setPersistence } from 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -12,4 +12,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
+setPersistence(auth, browserLocalPersistence)
 export const googleProvider = new GoogleAuthProvider()
